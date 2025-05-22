d49418dfe4b5a19df9d0527b1905e398bf9a9e01
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-22 09:51:33 -0300

    Segunda mudança

--- minfo.sh
+++ minfo.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "hi"
+
+[ "$1" = hostname ] && { hostname; exit; }
+[ "$1" = uptime ] && { uptime -p; exit; }
+[ "$1" = disk ] && { df -h /; exit; }
+[ "$1" = all ] && {hostname; uptime -p; df -h /; exit; } 
+
+echo "Uso $0 {hostname|uptime|disk|all}"
+exit 1
